675d35261c6fe8c2cb9a4d9a9324c6517bb9065c
KambaBalanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2021-06-08 02:24:08 -0400

    files

---
+++ Resto.js
@@ -0,0 +1,62 @@
+
+
+    //---------------TABLEAU DES REPAS------------------
+
+    var tabRepas=[{"nom":"Spaghetti","prix":8.95,"photo":"images/repas/spaghetti.jpg"},
+                  {"nom":"Lasagne","prix":9.95,"photo":"images/repas/lasagne.jpg"}];
+    //fonction remplissage des repas
+    function remplirSelRepas(){
+        var selRepas=document.querySelector('#selRepas');
+        selRepas.options[selRepas.options.length]=new Option("Votre choix")
+        for(unRepas of tabRepas){
+            selRepas.options[selRepas.options.length]=new Option(unRepas.nom,unRepas.nom.substring)
+        }
+    }
+    //fonction infos des repas
+    function infosRepasChoisi(selRepas){
+        var posRepasChoisi=selRepas.selectedIndex-1;
+        var repasChoisi=tabRepas[posRepasChoisi];
+        document.getElementById('prixRepas').innerHTML="<b>"+repasChoisi.prix+"</b>";
+        document.getElementById('photoRepas').src=repasChoisi.photo;
+        document.getElementById('photoRepas').style.display='block';
+    }       
+
+
+    //--------------TABLEAU DES ENTREES-------------------------
+
+    var tabEntree=[{"nom":"Salade","prix":5.45,"photo":"images/repas/salade.jpg"},
+                  {"nom":"Escargot","prix":4.95,"photo":"images/repas/escargot.jpg"}];
+    //fonction remplissage des entrees
+    function remplirSelEntree(){
+        var selEntree=document.querySelector('#selEntree');
+        selEntree.options[selEntree.options.length]=new Option("Votre choix")
+        for(unEntree of tabEntree){
+            selEntree.options[selEntree.options.length]=new Option(unEntree.nom,unEntree.nom.substring)
+        }
+    }
+    //fonction infos des entrees
+    function infosEntreeChoisi(selEntree){
+        var posEntreeChoisi=selEntree.selectedIndex-1;
+        var entreeChoisi=tabEntree[posEntreeChoisi];
+        document.getElementById('prixEntree').innerHTML="<b>"+entreeChoisi.prix+"</b>";
+        document.getElementById('photoEntree').src=entreeChoisi.photo;
+        document.getElementById('photoEntree').style.display='block';
+    }       
+
+    //bouton pour les calculs
+    $("button").on('click', function(e) {
+        var prixEntree = parseFloat($("#prixEntree").html()); 
+        var prixRepas = parseFloat($("#prixRepas").html()); 
+        var totalAvantTax = prixEntree + prixRepas
+        var totalTax = totalAvantTax * 0.14975
+        var totalApresTax = totalAvantTax + totalTax
+        
+        if (isNaN(prixEntree)) prixEntree = 0;
+        if (isNaN(prixRepas)) prixRepas = 0;
+        document.getElementById("facture").innerHTML = 
+        "Total: "+totalAvantTax.toFixed(2)+"$"+" taxes : "+totalTax.toFixed(2)+"$"+" = "+totalApresTax.toFixed(2)+"$";
+
+    });
+
+
+
